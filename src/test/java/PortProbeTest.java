b235b85f715ac6070bd727d597e87e4fb5063b56
Artur Signell <artur@vaadin.com>
2022-08-25 10:40:09 +0300

    Use one test only

--- src/test/java/PortProbeTest.java
+++ src/test/java/PortProbeTest.java
@@ -1,45 +1,25 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
 public class PortProbeTest {
-    // static {
-    //     ch.qos.logback.classic.Logger root = (Logger) LoggerFactory.getLogger("ROOT");
-    //     root.setLevel(Level.INFO);
-    // }
 
     @Test
-    public void probe1() {
-        runChromes();
-    }
-
-    @Test
-    public void probe2() {
-        runChromes();
-    }
-
-    @Test
-    public void probe3() {
-        runChromes();
-    }
-
-    @Test
-    public void probe4() {
-        runChromes();
-    }
-
-    @Test
-    public void probe5() {
-        runChromes();
-    }
-
-    private void runChromes() {
+    public void probe() {
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
-        for (int i = 0; i < 50; i++) {
-            ChromeDriver d = new ChromeDriver(options);
-            d.close();
+        int i = 0;
+        try {
+
+            for (i = 0; i < 1000; i++) {
+                ChromeDriver d = new ChromeDriver(options);
+                Object value = d.executeScript("return 'foo'");
+                d.quit();
+                Assert.assertEquals("foo", value);
+            }
+        } finally {
+            System.err.println("Was able to created " + i + " instances");
         }
     }
 
